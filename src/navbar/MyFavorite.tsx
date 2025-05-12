a9b7cc050d1b872ec08be9c9f3cf9a03a0d8baf6
Mustafa <raygal99@hotmail.com>
2025-05-12 08:16:46 +0200

    update sass

--- src/navbar/MyFavorite.tsx
+++ src/navbar/MyFavorite.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { FaHeart } from "react-icons/fa";
 import { FcReading } from "react-icons/fc";
 import { FaBookReader } from "react-icons/fa";
-
+import { TiDeleteOutline } from "react-icons/ti";
 
 
 function MyFavorite() {
@@ -14,63 +14,67 @@ function MyFavorite() {
     console.log("Something went wrong");
     return null;
   }
-  const { favorites, author, reading, toggleReading, renderStars } = context;
+  const { favorites, author, reading, toggleReading, renderStars, deleteHandler } = context;
 
 
 
 
   return (
-    <div className="favoriteBooks">
-      <div className="favoriteSection">
-        <section className="header">
-          <h2>My Favorite Book List</h2> <FaHeart style={{ color: "red", fontSize: "2em" }} />
-        </section>
-        <ol>
-          {author
-            .filter(book => favorites.includes(book.key))
-            .map(book => (
-              <li key={book.key} className="favoriteBook">
-                <span>{book.title}</span>
-                {book.cover_i && (
+    <div className="favoriteBooksContianer">
+      <section className="header">
+        <h2>My Favorite Book List</h2> <FaHeart style={{ color: "red", fontSize: "2em" }} />
+      </section>
+      <ol>
+        {author
+          .filter(book => favorites.includes(book.key))
+          .map(book => (
+            <li key={book.key} className="favoriteBooks">
+              <span>{book.title}</span>
+              {book.cover_i && (
+                <div className="deleteButton">
+                  <button onClick={() => deleteHandler(book.key)}><TiDeleteOutline /></button>
                   <img
                     src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
                     alt={book.title}
                   />
-                )}
-                <button
-                  onClick={() => toggleReading(book.key)}
-                  className="readButton"
-                >
-                  {reading.includes(book.key) ? <FcReading /> : <FaBookReader style={{ color: "black" }} />}
-                </button>
-                {reading.includes(book.key) && <div>Betyg: {renderStars(book.key)}</div>}
-              </li>
-            ))}
-        </ol>
-      </div>
+                </div>
+              )}
+              <button
+                onClick={() => toggleReading(book.key)}
+                className="readButton"
+              >
+                {reading.includes(book.key) ? <FcReading /> : <FaBookReader style={{ color: "black" }} />}
+              </button>
+              {reading.includes(book.key) && <div>Betyg: {renderStars(book.key)}</div>}
+            </li>
+          ))}
+      </ol>
 
-      <div className="readingSection">
-        <hr />
-        <section className="header">
-          <h2>My Read Book List</h2> <FcReading style={{ fontSize: "2em" }} />
-        </section>
-        <ol>
-          {author
-            .filter(book => reading.includes(book.key))
-            .map(book => (
-              <li key={book.key} className="favoriteBook">
-                <span>{book.title}</span>
-                {book.cover_i && (
+      <hr />
+
+      <section className="header">
+        <h2>My Read Book List</h2> <FcReading style={{ fontSize: "2em" }} />
+      </section>
+      <ol>
+        {author
+          .filter(book => reading.includes(book.key))
+          .map(book => (
+            <li key={book.key} className="favoriteBooks">
+              <span>{book.title}</span>
+              {book.cover_i && (
+                <div className="deleteButton">
+                  <button onClick={() => deleteHandler(book.key)}><TiDeleteOutline /></button>
                   <img
                     src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
                     alt={book.title}
                   />
-                )}
-                <div>Betyg: {renderStars(book.key)}</div>
-              </li>
-            ))}
-        </ol>
-      </div>
+                </div>
+              )}
+              <div>Betyg: {renderStars(book.key)}</div>
+            </li>
+          ))}
+      </ol>
+
     </div>
   );
 }
